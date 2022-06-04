6dbae16803dcf32dd1b0557a77b6a367e2933687
Ingrid Miranda <ingridmsousa0@gmail.com>
2022-06-04 19:34:22 -0300

    create header of CharactersPage

--- lib/view/characters_page/characters_page.dart
+++ lib/view/characters_page/characters_page.dart
@@ -26,6 +26,13 @@ class _CharactersPageState extends State<CharactersPage> {
   Widget build(BuildContext context) {
     return Observer(builder: (_) {
       return Scaffold(
+          appBar: AppBar(
+            backgroundColor: AppColors.colorDark,
+            title: const Text('SoftDesign Challenge'),
+            actions: [
+              IconButton(onPressed: () {}, icon: const Icon(Icons.menu))
+            ],
+          ),
           backgroundColor: AppColors.colorDark,
           body: /*  controller.charactersOptions == CharactersOption.show
             ? */
@@ -33,76 +40,103 @@ class _CharactersPageState extends State<CharactersPage> {
             direction: Axis.vertical,
             children: [
               Flexible(
-                  child: Container(
-                alignment: Alignment.bottomLeft,
-                padding: const EdgeInsets.only(left: 25, bottom: 8),
-                child: const Text(
-                  'Personagens',
-                  style: TextStyle(
-                      fontSize: 20,
-                      color: AppColors.colorPrimary,
-                      fontWeight: FontWeight.bold),
-                ),
-              )),
+                  flex: 3,
+                  child: Flex(
+                    direction: Axis.vertical,
+                    children: [
+                      Flexible(
+                        flex: 8,
+                        child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                  opacity: 0.8,
+                                  fit: BoxFit.fill,
+                                  image: AssetImage(
+                                    'lib/assets/images/marvel_background.jpg',
+                                  ))),
+                        ),
+                      ),
+                      Flexible(
+                        flex: 2,
+                        child: Container(
+                          alignment: Alignment.topLeft,
+                          padding: const EdgeInsets.only(left: 25, bottom: 8),
+                          child: const Text(
+                            'Personagens',
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: AppColors.colorPrimaryLight,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      )
+                    ],
+                  )),
               Flexible(
-                flex: 10,
+                flex: 7,
                 child: Padding(
                   padding:
                       const EdgeInsets.only(left: 25, right: 25, bottom: 15),
                   child: Container(
-                    decoration: const BoxDecoration(
-                        color: AppColors.colorGray,
-                        borderRadius: BorderRadius.all(Radius.circular(15))),
-                    child: ListView.builder(
-                        itemCount: 20, //controller.characters?.results.length,
-                        itemBuilder: (BuildContext buildContext, int index) {
-                          return Container(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 20, vertical: 15),
-                            child: Flex(
-                              direction: Axis.horizontal,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Flexible(
-                                  flex: 3,
-                                  child: CircleAvatar(
-                                    radius: 35,
-                                    backgroundImage: NetworkImage(
-                                        'http://i.annihil.us/u/prod/marvel/i/mg/3/40/4bb4680432f73/portrait_xlarge.jpg' /* '${controller.characters?.results[index].thumbnail.path}/portrait_small.${controller.characters?.results[index].thumbnail.extension}' */),
+                    decoration: BoxDecoration(
+                        color: AppColors.colorGray.withOpacity(0.9),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(10))),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: ListView.builder(
+                          itemCount:
+                              20, //controller.characters?.results.length,
+                          itemBuilder: (BuildContext buildContext, int index) {
+                            return Container(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 15),
+                              child: Flex(
+                                direction: Axis.horizontal,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Flexible(
+                                    flex: 3,
+                                    child: CircleAvatar(
+                                      radius: 35,
+                                      backgroundImage: NetworkImage(
+                                          'http://i.annihil.us/u/prod/marvel/i/mg/3/40/4bb4680432f73/portrait_xlarge.jpg' /* '${controller.characters?.results[index].thumbnail.path}/portrait_small.${controller.characters?.results[index].thumbnail.extension}' */),
+                                    ),
                                   ),
-                                ),
-                                const Flexible(
-                                    flex: 1, child: SizedBox(width: 30)),
-                                Flexible(
-                                  flex: 6,
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: const [
-                                      Text(
-                                        /* controller.characters?.results[index].name ??*/
-                                        "Nome não informado",
-                                        style: TextStyle(
-                                            fontSize: 16,
-                                            color: AppColors.colorLight,
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                      SizedBox(height: 8),
-                                      Text(
-                                        /* controller.characters?.results[index].name ??*/
-                                        "Descrição bal bal ajioh hoih hoih oih ioh ioh hoih ",
-                                        textAlign: TextAlign.justify,
-                                        style: TextStyle(
-                                            fontSize: 14,
-                                            color: AppColors.colorLight),
-                                      ),
-                                    ],
+                                  const Flexible(
+                                      flex: 1, child: SizedBox(width: 30)),
+                                  Flexible(
+                                    flex: 6,
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: const [
+                                        Text(
+                                          /* controller.characters?.results[index].name ??*/
+                                          "Nome não informado",
+                                          style: TextStyle(
+                                              fontSize: 16,
+                                              color: AppColors.colorLight,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        SizedBox(height: 8),
+                                        Text(
+                                          /* controller.characters?.results[index].name ??*/
+                                          "Descrição bal bal ajioh hoih hoih oih ioh ioh hoih ",
+                                          textAlign: TextAlign.justify,
+                                          style: TextStyle(
+                                              fontSize: 14,
+                                              color: AppColors.colorLight),
+                                        ),
+                                      ],
+                                    ),
                                   ),
-                                ),
-                              ],
-                            ),
-                          );
-                        }),
+                                ],
+                              ),
+                            );
+                          }),
+                    ),
                   ),
                 ),
               ),
